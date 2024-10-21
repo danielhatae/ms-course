d82c07a35cde24aebc2da69af2c251f10f13255b
danielhatae <daniel.hatae@gmail.com>
2024-10-21 16:38:57 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/daniel/hatae/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.daniel.hatae.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.daniel.hatae.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
