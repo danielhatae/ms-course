6dcbc0a657a9fdb37e2aae2e0ffdb5326710bf3d
danielhatae <daniel.hatae@gmail.com>
2024-10-23 12:57:33 -0300

    resilience4j fault tolerance

--- hr-payroll/src/main/java/com/daniel/hatae/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/daniel/hatae/hrpayroll/resources/PaymentResource.java
@@ -10,17 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.daniel.hatae.hrpayroll.entities.Payment;
 import com.daniel.hatae.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+	
 
 	@Autowired
 	private PaymentService service;
 	
+	@CircuitBreaker(name = "payments", fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
 	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days, Throwable throwable){
+		Payment payment = new Payment("Brann", 400.00, days);;
+		return ResponseEntity.ok(payment);
+	}	
 }
